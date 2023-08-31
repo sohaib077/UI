aec907e0b59dc39aa27359e9c6894fb2f237b8e8
Sohaib <89821455+sohaib077@users.noreply.github.com>
2023-08-31 21:36:16 +0300

    upgrading flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:ui/habit%20tracker/views/home%20layout/home%20layout%20view.dart';
 import 'package:ui/habit%20tracker/views/home%20view/cubits/home_cubit.dart';
 import 'package:ui/habit%20tracker/views/splash%20view/splash%20view.dart';
 
@@ -34,12 +33,15 @@ class MyApp extends StatelessWidget {
           child: MaterialApp(
             debugShowCheckedModeBanner: false,
             theme: ThemeData(
-                scaffoldBackgroundColor: kBackground1,
-                appBarTheme: const AppBarTheme(
-                    backgroundColor: Colors.white,
-                    systemOverlayStyle: SystemUiOverlayStyle(
-                        statusBarColor: Colors.transparent,
-                        statusBarIconBrightness: Brightness.dark))),
+              scaffoldBackgroundColor: kBackground1,
+              appBarTheme: const AppBarTheme(
+                backgroundColor: Colors.white,
+                systemOverlayStyle: SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent,
+                  statusBarIconBrightness: Brightness.dark,
+                ),
+              ),
+            ),
             home: const SplashView(),
             // home: const HomeLayoutView(),
           ),
